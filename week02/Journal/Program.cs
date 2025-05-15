57a9b1baae3009ede8095a8e61fd1f5a9871cd24
Kenneth Maberi <kennethmaberi@gmail.com>
2025-05-15 13:54:44 +0300

    saw

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,5 +1,24 @@
 using System;
 
+public class Entry
+{
+    public string Prompt { get; set; }
+    public string Response { get; set; }
+    public string Date { get; set; }
+
+    public Entry(string prompt, string response)
+    {
+        Prompt = prompt;
+        Response = response;
+        Date = DateTime.Now.ToShortDateString();
+    }
+
+    public override string ToString()
+    {
+        return $"{Date} - {Prompt}\n{Response}\n";
+    }
+}
+
 class Program
 {
     static void Main(string[] args)
